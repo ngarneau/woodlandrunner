0cfbe482dfcd9d4d119a26b35931ca20a369cc05
ngarneau <nicolas.garneau@duproprio.com>
2012-09-19 15:48:16 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woodlandrunner::Application.config.secret_token = '561149d489c169678e28a07b70930f314b88dbdb8c306899f0880631ee8d00c637981ae68aa9f4e6ba87b2e16a7ec7c8a15272c306097f2af1bbff6796813cbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Woodlandrunner::Application.config.session_store :cookie_store, :key => '_woodlandrunner_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Woodlandrunner::Application.config.session_store :active_record_store
